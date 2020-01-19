b2300694ec77774a16b7a6ca964a20ace51a39c6
YuliaStrakhova <Julia.Strahova@gmail.com>
2020-01-19 20:03:46 +0300

    df

--- lambda_function.py
+++ lambda_function.py
@@ -5,10 +5,13 @@ REGION = 'us-east-1'
 
 def lambda_handler(event, context):
     ec2 = boto3.resource('ec2', region_name=REGION)
+    print(1)
     for instance in ec2.instances.all():
+        print(2)
         print(instance)
+        print(3)
         ec2_cl = boto3.client('ec2', REGION)
-        ec2_cl.stop_instances(InstanceIds=[instance.id])
+        ec2_cl.terminate_instances(InstanceIds=[instance.id])
         print("Stoped\n\n")
 
 
