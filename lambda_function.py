ce5f2ad160edd32b10e43e91bca029d1074e0e6e
YuliaStrakhova <Julia.Strahova@gmail.com>
2020-01-19 23:25:53 +0300

    df

--- lambda_function.py
+++ lambda_function.py
@@ -1,36 +1,18 @@
 import boto3
-import functools
-import sys
 
 REGION = 'us-east-1'
 
 
-
-
-def log_event_on_error(handler):
-    @functools.wraps(handler)
-    def wrapper(event, context):
-        try:
-            return handler(event, context)
-        except Exception:
-            print('event = %r' % event)
-            raise
-
-    return wrapper
-
-
 def lambda_handler(event, context):
-    print(0)
+    print(1)
+
     ec2 = boto3.resource('ec2', region_name=REGION)
-    print(ec2)
+
+    print(ec2.instances.all())
     for instance in ec2.instances.all():
-        print(2)
-        print(instance)
-        print(3)
-        ec2_cl = boto3.client('ec2', REGION)
-        ec2_cl.terminate_instances(InstanceIds=[instance.id])
-        print("Stoped\n\n")
-
-
-
-
+       print(2)
+       print(instance)
+       print(3)
+       ec2_cl = boto3.client('ec2')
+       ec2_cl.terminate_instances(InstanceIds=[instance.id])
+       print("Stoped\n\n")
